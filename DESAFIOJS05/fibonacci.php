e056a149c16c752ed079b7c60658853ad7f45edd
Guilhermefariareis <64617103+Guilhermefariareis@users.noreply.github.com>
2020-09-14 20:20:57 -0300

    Add files via upload

---
+++ DESAFIOJS05/fibonacci.php
@@ -0,0 +1,15 @@
+<?php  
+$num = 15;  
+function series($num){  
+    if($num == 0){  
+    return 0;  
+    }else if( $num == 1){  
+return 1;  
+}  else {  
+return (series($num-1) + series($num-2));  
+}   
+}  
+for ($i = 0; $i < $num; $i++){  
+echo series($i);  
+echo "\n";  
+}
